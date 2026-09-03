37817391473aa1d3361f6c0b47a3c152bdf1a04a
Marcel Luchi Marchi <marcel.luchi@gmail.com>
2026-09-03 09:51:30 -0300

    Analise exploratoria

--- notebooks/import_dados.py
+++ notebooks/import_dados.py
@@ -7,7 +7,7 @@ ND = 'NAO DISPONIVEL'
 def get_col_order(col: str):
     COL_ORDER = {'dia_da_semana': ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'],
              'turno': ['MADRUGADA', 'MANHA', 'TARDE', 'NOITE'],
-             'fatalidade': ['NAO', 'SIM']}
+             'acidente_fatal': ['NAO', 'SIM']}
     
     return {'field': col, 'sort': COL_ORDER.get(col)}
 
@@ -15,10 +15,17 @@ def read_sinistros():
     sinistros_24 = pl.read_csv('dados/sinistros_2022-2024.csv', encoding='latin-1', separator=';').filter(pl.col('ano_sinistro') == 2024)
     sinistros_25 = pl.read_csv('dados/sinistros_2025-2026.csv', encoding='latin-1', separator=';').filter(pl.col('ano_sinistro') == 2025)
     qt_cols = [x for x in sinistros_24.columns if x.startswith('qtd_')]
-    return pl.concat([sinistros_24.with_columns(pl.col(qt_cols).cast(pl.Int16)), sinistros_25.with_columns(pl.col(qt_cols).cast(pl.Int16))])
+    return pl.concat([sinistros_24.with_columns(pl.col(qt_cols).cast(pl.Int16)), sinistros_25.with_columns(pl.col(qt_cols).cast(pl.Int16))]) \
+             #.filter(pl.col('tipo_registro') != 'NOTIFICACAO') \
+             #.filter(pl.col('tipo_local') == 'PUBLICO')
 
 
 def read_pessoas():
     pessoas_24 = pl.read_csv('dados/pessoas_2022-2024.csv', encoding='latin-1', separator=';').filter(pl.col('ano_sinistro') == 2024)
     pessoas_25 = pl.read_csv('dados/pessoas_2025-2026.csv', encoding='latin-1', separator=';').filter(pl.col('ano_sinistro') == 2025)
     return pl.concat([pessoas_24, pessoas_25])
+
+def read_veiculos():
+    veiculos_24 = pl.read_csv('dados/veiculos_2022-2024.csv', encoding='latin-1', separator=';').filter(pl.col('ano_sinistro') == 2024)
+    veiculos_25 = pl.read_csv('dados/veiculos_2025-2026.csv', encoding='latin-1', separator=';').filter(pl.col('ano_sinistro') == 2025)
+    return pl.concat([veiculos_24, veiculos_25])
